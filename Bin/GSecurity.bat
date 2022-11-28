586adba1e8fc3f22522bbd29cc3978a41e24c269
Gorstak-ZD <113023910+Gorstak-ZD@users.noreply.github.com>
2022-11-28 16:54:01 +0000

    Add files via upload

---
+++ Bin/GSecurity.bat
@@ -0,0 +1,24 @@
+:: Title
+Title GSecurity & color 0b
+echo == Installing GSecurity
+
+:: Active folder
+pushd %~dp0
+
+:: Debloat
+@powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0remove-default-apps.ps1"
+@powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Get-ProvisionedAppxPackage -Online | Remove-ProvisionedAppxPackage -Online"
+cleanmgr /sagerun:65535
+
+:: Policies
+Reg import HKCUPolicy.reg
+Reg import HKLMPolicy.reg
+
+:: Services
+Reg import Services.reg
+
+:: Performance
+Reg import Performance.reg
+
+:: Exit
+exit
