b86645236c42cb04f4de488bf52b0c884f6d1f89
Gorstak-ZD <113023910+Gorstak-ZD@users.noreply.github.com>
2022-12-04 22:29:29 +0100

    Update SetupComplete.cmd

--- Bin/SetupComplete.cmd
+++ Bin/SetupComplete.cmd
@@ -7,8 +7,8 @@ rd /s /q %windir%\system32\grouppolicy
 rd /s /q %windir%\system32\grouppolicyusers
 rd /s /q %windir%\syswow64\grouppolicy
 rd /s /q %windir%\syswow64\grouppolicyusers
-reg delete HKLM\Software\Policies
-reg delete HKCU\SOFTWARE\Policies
+reg delete HKLM\Software\Policies /f
+reg delete HKCU\SOFTWARE\Policies /f
 
 :: Import policies
 lgpo /g %~dp0
