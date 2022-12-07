bfee562f7befbfec1bde25b178ccaa4bf8bf8ccf
Gorstak-ZD <113023910+Gorstak-ZD@users.noreply.github.com>
2022-12-07 10:43:49 +0100

    Add files via upload

---
+++ Setup.cmd
@@ -0,0 +1,25 @@
+<!-- : Begin batch script
+@cls
+@echo off
+>nul chcp 437
+setlocal enabledelayedexpansion
+title GSecurity & color 0b
+>nul 2>&1 where powershell || (
+	echo.
+	echo Missing Critical files [powershell.exe]
+	echo.
+	pause
+	exit /b
+)
+fsutil dirty query %systemdrive% >nul
+if %errorlevel% == 0 (
+    goto:start
+) else (
+    call :IsAdmin
+) 
+:start
+call %~dp0\Bin\SetupComplete.cmd
+exit
+:IsAdmin
+@powershell.exe Start-Process %~f0 -verb runas
+exit
