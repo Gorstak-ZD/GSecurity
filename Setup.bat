95f8a0f8cf6929c8a51350b728735a1c6a782e82
Gorstak-ZD <113023910+Gorstak-ZD@users.noreply.github.com>
2022-12-20 19:51:37 +0100

    Add files via upload

--- Setup.bat
+++ Setup.bat
@@ -1,25 +1,6 @@
-<!-- : Begin batch script
-@cls
-@echo off
->nul chcp 437
-setlocal enabledelayedexpansion
-title GSecurity & color 0b
->nul 2>&1 where powershell || (
-	echo.
-	echo Missing Critical files [powershell.exe]
-	echo.
-	pause
-	exit /b
-)
-fsutil dirty query %systemdrive% >nul
-if %errorlevel% == 0 (
-    goto:start
-) else (
-    call :IsAdmin
-) 
-:start
+@ECHO OFF
+
+>nul 2>&1 fsutil dirty query %systemdrive% || echo CreateObject^("Shell.Application"^).ShellExecute "%~0", "ELEVATED", "", "runas", 1 > "%temp%\uac.vbs" && "%temp%\uac.vbs" && exit /b
+DEL /F /Q "%temp%\uac.vbs"
+
 call %~dp0\Bin\GSecurity.bat
-exit
-:IsAdmin
-@powershell.exe Start-Process %~f0 -verb runas
-exit
